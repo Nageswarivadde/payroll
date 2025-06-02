cf6cc7d439e5ed4ac37c67d39054a7de650ddefa
Nageswarivadde <nageswarivadde2@gmail.com>
2025-06-02 20:35:14 +0530

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
-MONGO_URI = "mongodb+srv://nageswarivadde2:GJCScnaUSnUvXlcy@cluster0.zaxcx89.mongodb.net/"
+MONGO_URI = "mongodb+srv://nageswarivadde2:Nageswari%40098@cluster0.zaxcx89.mongodb.net/"
 DB_NAME = "payrollDB"
+
